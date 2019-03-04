ba0db8aab55177f66ea4b4081e0caf6f3b8a34ee
nilo <dh@gocept.com>
2019-03-04 20:08:34 +0100

    fix logging

--- migrate.py
+++ migrate.py
@@ -8,6 +8,13 @@ from config import \
 	CODIMD_BASE, CODIMD_LOGIN, CODIMD_PASSWORD, \
 	PG_HOST, PG_USER, PG_PASS, PG_DBNAME
 
+parser = argparse.ArgumentParser()
+parser.add_argument('pad_name', nargs=1, help='Name of the Pad to migrate.')
+args = parser.parse_args()
+pad_name = args.pad_name[0]
+
+LOG_FORMAT = '%(asctime)-15s - %(msg)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
 log = logging.getLogger('Migrator')
 
 log.info('Connecting to {}@{} using user {}.'.format(
@@ -19,34 +26,29 @@ db_conn = psycopg2.connect(dbcon_str)
 
 def check_result(url, result):
 	if result.status_code != 200:
-		log.error("Request to {} failed.".format(url))
+		log.error("Request to {} failed".format(url))
 		sys.exit(0)
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument('pad_name', nargs=1, help='Name of the Pad to migrate.')
-args = parser.parse_args()
-pad_name = args.pad_name[0]
 
 
 log.info('Migrating pad {}.'.format(pad_name))
 
 
 login_url = '{}/login'.format(CODIMD_BASE)
-log.info('Logging into codimd at {}.'.format(login_url))
+log.info('Logging into codimd at {}'.format(login_url))
 codimd_session = requests.Session()
 data = {"email":CODIMD_LOGIN, "password":CODIMD_PASSWORD}
 result = codimd_session.post(login_url, data=data)
 check_result(login_url, result)
 
 export_url = '{}/{}/export/txt'.format(ETHERPAD_BASE, pad_name)
-log.info('Fetching old pad content at {}.'.format(export_url))
+log.info('Fetching old pad content at {}'.format(export_url))
 result = requests.get(export_url)
 check_result(export_url, result)
 pad_content = result.text
 
 new_url = '{}/{}'.format(CODIMD_BASE, pad_name)
-log.info('Creating new pad in codimd at {}.'.format(new_url))
+log.info('Creating new pad in codimd at {}'.format(new_url))
 result = codimd_session.get(new_url)
 check_result(new_url, result)
 
