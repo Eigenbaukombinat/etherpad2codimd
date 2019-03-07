e6837598edb2006a7542609a459d83dd46ba6400
nilo <dh@gocept.com>
2019-03-07 16:47:29 +0100

    wip

--- migrate.py
+++ migrate.py
@@ -3,10 +3,15 @@ import requests
 import logging
 import psycopg2
 import sys
+import re
+from htmlmd import html2md
 from config import \
 	ETHERPAD_BASE, \
 	CODIMD_BASE, CODIMD_LOGIN, CODIMD_PASSWORD, \
 	PG_HOST, PG_USER, PG_PASS, PG_DBNAME
+
+
+regex = re.compile(r"^\ \ \ \ ")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('pad_name', nargs=1, help='Name of the Pad to migrate.')
@@ -34,28 +39,40 @@ def check_result(url, result):
 log.info('Migrating pad {}.'.format(pad_name))
 
 
-login_url = '{}/login'.format(CODIMD_BASE)
-log.info('Logging into codimd at {}'.format(login_url))
-codimd_session = requests.Session()
-data = {"email":CODIMD_LOGIN, "password":CODIMD_PASSWORD}
-result = codimd_session.post(login_url, data=data)
-check_result(login_url, result)
-
 export_url = '{}/{}/export/txt'.format(ETHERPAD_BASE, pad_name)
 log.info('Fetching old pad content at {}'.format(export_url))
 result = requests.get(export_url)
 check_result(export_url, result)
+
+
+
 pad_content = result.text
+# convert tabs to spaces
+new_pad_content = pad_content.replace('\t', '    ')
+lines = []
+# remove extra whitespace at the beginning of lines
+for line in new_pad_content.splitlines():
+	if line.startswith('    '):
+		line = line[4:]
+	lines.append(line)
+
+new_pad_content = '\n'.join(lines)
 
 new_url = '{}/{}'.format(CODIMD_BASE, pad_name)
 log.info('Creating new pad in codimd at {}'.format(new_url))
 result = codimd_session.get(new_url)
 check_result(new_url, result)
 
+# for now, print the result
+
+print(new_pad_content)
+
+
+
 # set content in db
-cursor = db_conn.cursor()
-cursor.execute('UPDATE "Notes" set content = %s where alias = %s', (
-	pad_content, pad_name))
-db_conn.commit()
-cursor.close()
-db_conn.close()
+#cursor = db_conn.cursor()
+#cursor.execute('UPDATE "Notes" set content = %s where alias = %s', (
+#	pad_content, pad_name))
+#db_conn.commit()
+#cursor.close()
+#db_conn.close()
